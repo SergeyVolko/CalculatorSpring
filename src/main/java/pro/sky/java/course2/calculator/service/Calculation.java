d0e5c37d8f052c9725693f9a91f7fc828daa6d3f
Lenovo <rusobraz@mail.ru>
2023-09-05 16:36:50 +0300

    ДЗ №4 Курс Java Core(исправил)

--- src/main/java/pro/sky/java/course2/calculator/service/Calculation.java
+++ src/main/java/pro/sky/java/course2/calculator/service/Calculation.java
@@ -1,9 +1,8 @@
 package pro.sky.java.course2.calculator.service;
 
 public interface Calculation {
-    String getSalute();
-    String plusAction(String num1, String num2);
-    String minusAction(String num1, String num2);
-    String multiplyAction(String num1, String num2);
-    String divideAction(String num1, String num2);
+    int plusAction(String num1, String num2);
+    int minusAction(String num1, String num2);
+    int multiplyAction(String num1, String num2);
+    int divideAction(String num1, String num2);
 }
